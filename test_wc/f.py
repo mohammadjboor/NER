7d2f28cf661c603f33dfb447048bd4c09a560f28
mohammadjboor <mjboor26@yahoo.com>
2017-12-18 10:53:48 +0200

    rm

--- test_wc/f.py
+++
@@ -1,8 +0,0 @@
-with open("firsttoken", "w") as ft, open("ANERCorp", "r") as src:
-    lines = src.readlines()
-    for l in lines:
-        tokens = l.strip().split(" ")
-        word = tokens[0]
-        #print(word)
-        #import pdb; pdb.set_trace()
-        ft.write(word +'\n')
--- tf-idf.py
+++
@@ -1,9 +0,0 @@
-from sklearn.feature_extraction.text import CountVectorizer
-train_set = ("The sky is blue.", "The sun is bright.")
-test_set = ("The sun in the sky is bright.",
-"We can see the shining sun, the bright sun.")
-
-
-
-
- CountVectorizer(max_df=0.95, min_df=2, max_features=n_features, stop_words='is')
